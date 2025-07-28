8bec2ccd2ef750b7a88abe64a9af75a9d34b082e
DaoCongVinh <22130325@st.hcmuaf.edu.vn>
2025-07-28 15:07:56 +0700

    thanh toan vnpay

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -29,7 +29,7 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
